0911fb8bc17f68fbef4b3f47e4ed10ea5242f8c6
Larry <tradesouthwest@outlook.com>
2026-01-18 16:22:48 -0700

    Add files via upload

---
+++ includes/class-wc-product-simple.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Simple Product Class.
+ *
+ * The default product type kinda product.
+ *
+ * @package ClassicCommerce/Classes/Products
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Simple product class.
+ */
+class WC_Product_Simple extends WC_Product {
+
+	/**
+	 * Initialize simple product.
+	 *
+	 * @param WC_Product|int $product Product instance or ID.
+	 */
+	public function __construct( $product = 0 ) {
+		$this->supports[] = 'ajax_add_to_cart';
+		parent::__construct( $product );
+	}
+
+	/**
+	 * Get internal type.
+	 *
+	 * @return string
+	 */
+	public function get_type() {
+		return 'simple';
+	}
+
+	/**
+	 * Get the add to url used mainly in loops.
+	 *
+	 * @return string
+	 */
+	public function add_to_cart_url() {
+		$url = $this->is_purchasable() && $this->is_in_stock() ? remove_query_arg( 'added-to-cart', add_query_arg( 'add-to-cart', $this->id ) ) : get_permalink( $this->id );
+		
+		return apply_filters( 'woocommerce_product_add_to_cart_url', $url, $this );
+	}
+
+	/**
+	 * Get the add to cart button text.
+	 *
+	 * @return string
+	 */
+	public function add_to_cart_text() {
+		$text = $this->is_purchasable() && $this->is_in_stock() ? __( 'Add to cart', 'classic-commerce' ) : __( 'Read more', 'classic-commerce' );
+
+		return apply_filters( 'woocommerce_product_add_to_cart_text', $text, $this );
+	}
+
+	/**
+	 * Get the add to cart button text description - used in aria tags.
+	 *
+	 * @since  WC-3.3.0
+	 * @return string
+	 */
+	public function add_to_cart_description() {
+		/* translators: %s: Product title */
+		$text = $this->is_purchasable() && $this->is_in_stock() ? __( 'Add to cart &ldquo;%s&rdquo;', 'classic-commerce' ) : __( 'Read more about &ldquo;%s&rdquo;', 'classic-commerce' );
+
+		return apply_filters( 'woocommerce_product_add_to_cart_description', sprintf( $text, $this->get_name() ), $this );
+	}
+}
